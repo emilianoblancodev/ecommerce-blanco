daf282a35da4a38529f93dd0591ab083945d4506
= <=>
2022-02-15 09:13:27 -0300

    meto en el objeto la ruta src de las imagenes

--- src/components/stock.js
+++ src/components/stock.js
@@ -1,61 +1,74 @@
 const stock = [{
     id: 0,
     nombre: "Top Nike",
+    src: "../src/assets/img/top.png",
     descripcion: "Top de color rosa intenso de licra especial para hacer running",
 },
 {
     id: 1,
     nombre: "Mochila Adidas",
+    src: "../src/assets/img/mochila.png",
     descripcion: "Mochila Adidas de color gris plomo, impermeable con bolsillo de seguridad antirrobo ",
 },
 {
     id: 2,
     nombre: "Remera Adidas",
+    src: "../src/assets/img/remera_adidas.png",
     descripcion: "Remera de hombre Adidas, de lycra transpirable especial para running",
 },
 {
     id: 3,
     nombre: "Gorro Nike",
+    src: "../src/assets/img/gorro.png",
     descripcion: "Gorro Nike de color blanco, transpirable",
 },
 {
     id: 4,
     nombre: "Pantalón Adidas",
+    src: "../src/assets/img/pantalon.png",
     descripcion: "Pantalón de hombre Adidas de color azul chupinado con cierre bajo",
 },
 {
     id: 5,
     nombre: "Short Nike",
+    src: "../src/assets/img/short.png",
     descripcion: "Short de mujer de color rosa, transpirable",
 },
 {
     id: 6,
     nombre: "Campera Nike",
+    src: "../src/assets/img/campera.png",
     descripcion: "Campera de hombre Nike, de color negro con capucha y cierre ultra-resistente",
 },
 {
     id: 7,
     nombre: "Zapatilla Nike",
+    src: "../src/assets/img/zapatilla_nike.png",
     descripcion: "Zapatilla de hombre modelo Renew, de color blanco con suela ultra blanda especial para running",
 },
 {
     id: 8,
     nombre: "Zapatilla Nike",
+    src: "../src/assets/img/nike_airmax.png",
     descripcion: "Zapatilla Nike modelo AIR-MAX de hombre, de color negro y suela especial con capsula de aire",
 },
 {
     id: 9,
     nombre: "Remera DPS",
+    src: "../src/assets/img/remera.png",
     descripcion: "Remera Running de hombre de color azul transpirable",
 },
 {
     id: 10,
     nombre: "Zapatilla Puma",
+    src: "../src/assets/img/zapatilla_puma.png",
     descripcion: "Zapatilla Puma de color gris de hombre con suela estilo clasico especial urban ",
 },
 {
     id: 11,
     nombre: "Calzas Nike",
+    src: "../src/assets/img/calzas.png",
     descripcion: "Calzas Nike largas de lycra con inscripcion de la marca en la pierna",
 },    
 ]
+
