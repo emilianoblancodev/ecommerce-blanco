f49cc36881091672759d5d85a5bd74198353185f
= <=>
2022-02-15 09:05:13 -0300

    Creo el objeto con los diferentes productos para mostrar en las cards

---
+++ src/components/stock.js
@@ -0,0 +1,61 @@
+const stock = [{
+    id: 0,
+    nombre: "Top Nike",
+    descripcion: "Top de color rosa intenso de licra especial para hacer running",
+},
+{
+    id: 1,
+    nombre: "Mochila Adidas",
+    descripcion: "Mochila Adidas de color gris plomo, impermeable con bolsillo de seguridad antirrobo ",
+},
+{
+    id: 2,
+    nombre: "Remera Adidas",
+    descripcion: "Remera de hombre Adidas, de lycra transpirable especial para running",
+},
+{
+    id: 3,
+    nombre: "Gorro Nike",
+    descripcion: "Gorro Nike de color blanco, transpirable",
+},
+{
+    id: 4,
+    nombre: "Pantalón Adidas",
+    descripcion: "Pantalón de hombre Adidas de color azul chupinado con cierre bajo",
+},
+{
+    id: 5,
+    nombre: "Short Nike",
+    descripcion: "Short de mujer de color rosa, transpirable",
+},
+{
+    id: 6,
+    nombre: "Campera Nike",
+    descripcion: "Campera de hombre Nike, de color negro con capucha y cierre ultra-resistente",
+},
+{
+    id: 7,
+    nombre: "Zapatilla Nike",
+    descripcion: "Zapatilla de hombre modelo Renew, de color blanco con suela ultra blanda especial para running",
+},
+{
+    id: 8,
+    nombre: "Zapatilla Nike",
+    descripcion: "Zapatilla Nike modelo AIR-MAX de hombre, de color negro y suela especial con capsula de aire",
+},
+{
+    id: 9,
+    nombre: "Remera DPS",
+    descripcion: "Remera Running de hombre de color azul transpirable",
+},
+{
+    id: 10,
+    nombre: "Zapatilla Puma",
+    descripcion: "Zapatilla Puma de color gris de hombre con suela estilo clasico especial urban ",
+},
+{
+    id: 11,
+    nombre: "Calzas Nike",
+    descripcion: "Calzas Nike largas de lycra con inscripcion de la marca en la pierna",
+},    
+]
