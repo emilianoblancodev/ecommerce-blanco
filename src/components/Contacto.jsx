b706ce404cca51d16541475b4cc04b7ae7cb336d
= <=>
2022-02-16 15:14:46 -0300

    Me quedo colgado subir el componente jeje

---
+++ src/components/Contacto.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contacto = () => {
+  return (
+    <>
+      <div className="container mb-3  mt-3 w-50">
+        <label for="exampleFormControlInput1" class="form-label">Nombre y Apellido</label>
+        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="nombre@dominio.com" />
+      </div>
+      <div className="container mb-3  mt-3 w-50">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="nombre@dominio.com" />
+      </div>
+      <div className="container mb-3 w-50">
+        <label for="exampleFormControlTextarea1" className="form-label">Ingrese su consulta</label>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <button className="btn btn-primary mt-3">Enviar</button>
+      </div>
+    </>
+  )
+}
+
+export default Contacto
