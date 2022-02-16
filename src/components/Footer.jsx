34233f45d0a3d5cccb30f31fce69600098be49ba
= <=>
2022-02-16 15:12:51 -0300

    Preparo para crear en enlace al formulario de contacto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,14 @@ const Footer = () => {
   return (
     <>
         <nav className="navbar fixed-bottom navbar-light bg-light">
-            <div className="container-fluid d-flex justify-content-center">
-                <p>Hecho en<a className="navbar-brand mb-0" href="#"><img id="react_logo" src={react_logo}/></a></p>
+            <div className="container-fluid d-flex">
+              <p>Contacte con nosotros</p>
             </div>
+            
+            <div className="container-fluid d-flex justify-content-center">
+                <p>Hecho en<a className="navbar-brand mb-0" href="#"><img id="react_logo" src={react_logo}/></a></p>   
+            </div>
+                
         </nav>
 
     </>
