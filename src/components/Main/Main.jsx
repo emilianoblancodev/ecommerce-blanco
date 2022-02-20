ea8291a0ff66cf5ca87d3837103f7c91022b17ad
= <=>
2022-02-20 16:26:17 -0300

    Linkeo Main.css y elijo el color de fondo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 import Carrousel from '../Carrousel/Carrousel.jsx'
 import Tarjeta from '../Tarjeta/Tarjeta.jsx'
+import './Main.css'
 
 const Main = () => {
   return (
